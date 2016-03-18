088e697959e3db43592486e359508bc64c65aad8
msmith9393 <megan@tokaypress.com>
2016-03-18 12:20:38 -0700

    Grocery list without classes (stateless)

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-// TODO
+// app component
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList items={['Cucumber', 'Kale', 'Apple']} />
+  </div>
+);
+
+// grocery list component
+var GroceryList = (props) => {
+  return (
+    <ul>
+      {props.items.map(item =>
+        <GroceryListItem item={item} />
+      )}
+    </ul>
+  );
+};
+
+
+// grocery list item componenet
+var GroceryListItem = (props) => {
+  var state = {
+    hover: false
+  }
+  var onListItemHover = (event) => {
+    if (!state.hover) {
+      state.hover = true;
+    } else {
+      state.hover = false;
+    }
+  };
+
+  var style = {
+    fontWeight: state.hover ? 'bold' : 'normal'
+  }
+
+  return (
+    <li style={style} onMouseOver={onListItemHover}>{props.item}</li>
+  );
+
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
